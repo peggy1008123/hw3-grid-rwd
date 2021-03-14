95860a7dbf65ff83c7f5fa12933bc72caf5160b0
peggy1008123 <peggy1008123@yahoo.com.tw>
2021-03-15 03:30:56 +0800

    href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 export default function Header() {
   return (
       <ul className="Nav">
-          <li><a href="#" className="Nav-item text-light opacity-3">Tableware</a></li>
-          <li><a href="#" className="Nav-item text-light opacity-3">Cookware</a></li>
-          <li><a href="#" className="Nav-item text-light opacity-3">Home accessories</a></li>
-          <li><a href="#" className="Nav-item text-light opacity-3">Lighting</a></li>
-          <li><a href="#" className="Nav-item text-light opacity-3">Textile</a></li>
-          <li><a href="#" className="Nav-item text-light opacity-3">Furniture</a></li>
+          <li><a href="https://github.com/peggy1008123/hw3-grid-rwd/tree/main/src" className="Nav-item text-light opacity-3">Tableware</a></li>
+          <li><a href="https://github.com/peggy1008123/hw3-grid-rwd/tree/main/src" className="Nav-item text-light opacity-3">Cookware</a></li>
+          <li><a href="https://github.com/peggy1008123/hw3-grid-rwd/tree/main/src" className="Nav-item text-light opacity-3">Home accessories</a></li>
+          <li><a href="https://github.com/peggy1008123/hw3-grid-rwd/tree/main/src" className="Nav-item text-light opacity-3">Lighting</a></li>
+          <li><a href="https://github.com/peggy1008123/hw3-grid-rwd/tree/main/src" className="Nav-item text-light opacity-3">Textile</a></li>
+          <li><a href="https://github.com/peggy1008123/hw3-grid-rwd/tree/main/src" className="Nav-item text-light opacity-3">Furniture</a></li>
       </ul>
   );
 }
